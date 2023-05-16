7b1c8ad68e6e855258561b278b6ca93569c14e76
lucastocker <lucastocker@gmail.com>
2023-05-16 09:26:47 +0200

    aggiornamento

--- Esercizio_3/1_pong/sketch.js
+++ Esercizio_3/1_pong/sketch.js
@@ -1,34 +1,47 @@
 // lascia che la posizione X sia 200, sostituiamo dei valori con dei contenitori di valori
 let posizioneX
 let posizioneY
-let velX = 5
-let velY = 3
+let velX = 7
+let velY = 5
 
 function setup(){
-	createCanvas(800, 400)
+	createCanvas(windowWidth/1.5, windowHeight/1.5)
 	posizioneX = width/2
 	posizioneY = height/2
-	background(200, 0, 200)
-	velX = random(-5, 5)
-	velY = random(-5, 5)
+	background(0, 0, 0)
+	velX = random(-8, 10)
+	velY = random(-8, 10)
 }
+
 
 function draw(){
-	noStroke()
-	fill(random(200, 255), random(100), random(255))
+
+
+		noStroke()
+		fill(((sin(frameCount * 0.1) + 1.1) * 110), ((sin(frameCount * 0.1) + 1.1) * 110), ((sin(frameCount * 0.1) + 1.1) * 100))
+		
+		
+		ellipse(posizioneX, posizioneY,55, 55)
+		
+		
+		posizioneX = posizioneX + velX
+		posizioneY = posizioneY + velY
+		
+		if (posizioneX >= width || posizioneX <= 0) velX = -velX
+		if (posizioneY >= height || posizioneY <= 0) velY = -velY
+		
+		
+
 	
-	ellipse(posizioneX, posizioneY, 30, 30)
-
-	posizioneX = posizioneX + velX
-	posizioneY = posizioneY + velY
-
-	if (posizioneX >= width || posizioneX <= 0) velX = -velX
-	if (posizioneY >= height || posizioneY <= 0) velY = -velY
-
+	
 }
+
+
+
 	
 function keyPressed(){
 
 	save("pong.png")
+
 	
 }
--- Esercizio_3/2_pioggia/sketch.js
+++ Esercizio_3/2_pioggia/sketch.js
@@ -6,13 +6,16 @@ function draw(){
 	
 	background(0,0,0)
 
-	for(let i=0; i<50; i=i+1){
-		let spessore = random(1, 3)
-		let lunghezza = random(20, 100)
+	for(let i=0; i<150; i=i+1){
+		let spessore = random(0.3, 2.5)
+		let lunghezza = random(40, 130)
 		let posX = random(0, width)
 		let posY = random(-lunghezza, height)
-		stroke(255)
+
+		stroke(255, 255, 250)
 		strokeWeight(spessore)
-		line(posX, posY, posX, posY + lunghezza)
+		line(random(posX, posX-5), posY, posX, posY + lunghezza)
+
+		
 	}
-}
+}	
